3a77af3bcddda6ba7ecfed8340e1e13e7a888c11
Justin Hilliard <justin.a.hilliard@gmail.com>
2013-05-16 21:43:26 -0700

    Initial Commit

---
+++ CrystalBall/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CrystalBall
+//
+//  Created by Justin Hilliard on 5/16/13.
+//  Copyright (c) 2013 Justin Hilliard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CrystalBall/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CrystalBall
+//
+//  Created by Justin Hilliard on 5/16/13.
+//  Copyright (c) 2013 Justin Hilliard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
